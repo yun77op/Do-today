dce95d2afd058a56daf82803040ba0ba59130f1d
yun77op <yun77op@gmail.com>
2011-12-11 21:35:10 +0800

    login page

--- web.js
+++ web.js
@@ -1,57 +1,63 @@
 var express = require('express'),
         ejs = require('ejs'),
-     Oauth2 = require('./lib/oauth2'),
+ mongoStore = require('connect-mongodb'),
+     Oauth2 = require('./lib/oauth2').Oauth2,
      config = require('./lib/config').config;
 
 var app = express.createServer();
 
+app.configure('development', function() {
+  app.set('db-uri', 'mongodb://localhost/dotoday-development');
+  app.use(express.errorHandler({ dumpExceptions: true }));
+});
 
-app.name = 'dotoday';
-app.version = '0.1';
+app.configure('production', function() {
+  app.set('db-uri', 'mongodb://localhost/dotoday-production');
+});
 
 app.configure(function () {
   app.use(express.logger());
-  app.use(express.static(__dirname + '/public'));
   app.set('view engine', 'ejs');
   app.set('views', __dirname + '/views');
+  app.use(express.cookieParser());
   app.use(express.session({
-    store: mongoStore(),
-    secret: app.name + '_secret',
+    store: mongoStore(app.set('db-uri')),
+    secret: 'topsecret',
     cookie: {
       maxAge : config.server.cookie_maxAge
     }
   }));
+  app.use(app.router);
+  app.use(express.static(__dirname + '/public'));
 });
 
+app.configure('development', function() {
+  app.use(express.errorHandler({ dumpExceptions: true }));
+});
 
 var oauth = new Oauth2(config.oauth.client_id, config.oauth.client_secret);
 
-function initRouter() {
-  var routerMap = {
-    '/': {
-      title: 'Do today',
-      template: 'login'
-    },
-
-    '/app': {
-      title: 'Do today',
-      template: 'app'
-    }
-  };
-  var router;
-
-  for (var path in routerMap) {
-    router = routerMap[path];
-    app.get(path, loadUser, function (req, res) {
-      res.render(router.template, {
-        locals: {
-          title: router.title,
-          currentUser: req.currentUser
-        }
-      });
+app.get('/', loadUser, function (req, res) {
+  if (req.currentUser) {
+    res.redirect('/app');
+  } else {
+    res.render('login', {
+      locals: {
+        title: '登陆Dotoday'
+      }
     });
   }
-}
+});
+
+app.get('/app', loadUser, function (req, res) {
+  var currentUser = req.currentUser;
+  res.render('app', {
+    locals: {
+      title: currentUser.name + ' - Dotoday',
+      currentUser: currentUser
+    }
+  });
+});
 
 app.get('/authorize', function (req, res) {
   oauth2.redirectUri = res.headers.protocol + res.headers.host + '/callback';
@@ -67,11 +73,14 @@ app.get('/callback', function (req, res) {
   });
 });
 
-app.get('/logout', function (req, res) {
-  
+app.get('/logout', function (req, res, next) {
+  req.session.destroy(function (err) {
+    if (err) return next(err);
+    res.redirect('home');
+  });
 });
 
-initRouter();
+
 var port = process.env.PORT || 3000;
 app.listen(port, function () {
   console.log("Listening on " + port);
@@ -79,10 +88,14 @@ app.listen(port, function () {
 
 function loadUser(req, res, next) {
   var user = req.session.userToken;
+  console.log('loadUser');
+  console.log(req.url);
   if (user) {
-    req.currentUser = JSON.parse(user);
+    req.currentUser = user;
     next();
   } else {
+    if (req.url == '/')
+      return next();
     res.redirect('home');
   }
 }
