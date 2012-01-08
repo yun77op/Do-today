95b83a73fb7d0f0cd1d2feed0e5a96c51ee9fed5
yun77op <yun77op@gmail.com>
2012-01-08 17:08:39 +0800

    task more actions overlay

--- public/scripts/jquery.overlay.js
+++ public/scripts/jquery.overlay.js
@@ -8,85 +8,83 @@
 
 (function($) {
 
-	$.widget( 'ui.overlay', {
-		version: '@VERSION',
-		options: {
-			event: 'click',
-			srcNode: null,
-			autoOpen: false,
-			position: {
-				at: 'left top',
-				my: 'right bottom'
-			}
-		},
+  $.widget( 'ui.overlay', {
+    version: '@VERSION',
+    options: {
+      event: 'click',
+      srcNode: null,
+      autoOpen: false,
+      position: {
+        at: 'left top',
+        my: 'right bottom'
+      }
+    },
 
-		_create: function() {
-			var options = this.options;
-			if (!options.srcNode) {
-				options.srcNode = '<div class="ui-overlay"></div>';
-			}
-			options.srcNode = $(options.srcNode).appendTo('body');
-			this._setupEvents(options.event);
+    _create: function() {
+      var options = this.options;
+      options.srcNode = $(options.srcNode);
+      this._setupEvents(options.event);
+      if (!options.autoOpen) {
+        this.close();
+      }
+    },
 
-			if (options.autoOpen) { return; }
-			this.close();
-		},
-		
-		_setOption: function(key, value) {
-			switch (key) {
-				case 'event':
-					this._setupEvents(value);
-					break;
-				case 'zIndex':
-					this.element.css('z-index', value);
-				case 'position':
-					$.extend(this.options.position, value);
-					this._position();
-					return;
-			}
-			this._super('_setOption', key, value);
-		},
+    _setOption: function(key, value) {
+      switch (key) {
+        case 'event':
+          this._setupEvents(value);
+          break;
+        case 'zIndex':
+          this.element.css('z-index', value);
+          break;
+        case 'position':
+          $.extend(this.options.position, value);
+          this._position();
+          return;
+      }
+      this._super('_setOption', key, value);
+    },
 
-		_setupEvents: function(event) {
-			this.element.unbind( ".overlay" ).bind( event.split( " " ).join( ".overlay " ) + ".overlay",
-					$.proxy( this, "open" ) );
+    _setupEvents: function(event) {
+      this.element.unbind( ".overlay" ).bind( event.split( " " ).join( ".overlay " ) + ".overlay",
+          $.proxy( this, "open" ) );
 
-			// disable click in any case
-			this.element.bind( "click.overlay", function( e ) {
-				e.preventDefault();
-				e.stopPropagation();
-			});
-		},
-		
-		_position: function() {
-			var option = $.extend({
-				of: this.element
-			}, this.options.position);
+      // disable click in any case
+      this.element.bind( "click.overlay", function( e ) {
+        e.preventDefault();
+        e.stopPropagation();
+      });
+    },
+    
+    _position: function() {
+      var option = $.extend({
+        of: this.element
+      }, this.options.position);
 
-			this.options.srcNode.position(option);
-		}
-	});
+      this.options.srcNode.position(option);
+    }
+  });
 
 
-	$.extend($.ui.overlay.prototype, {
-		'open': function() {
-			var self = this,
-					srcNode = this.options.srcNode;
-			srcNode.show();
-			this._position();
-			this._trigger('open');
-			$(document).bind('click.overlay', function(e) {
-				if ($(e.target).closest(srcNode).length == 0) {
-					$(this).unbind(e);
-					self.close();
-				}
-			});
-		},
+  $.extend($.ui.overlay.prototype, {
+    'open': function() {
+      var self = this,
+          srcNode = this.options.srcNode;
+      srcNode.show();
+      this._position();
+      this._trigger('open');
+      $(document).bind('click.overlay', function(e) {
+        if ($(e.target).closest(srcNode).length === 0) {
+          $(this).unbind(e);
+          self.close();
+        }
+      });
+    },
 
-		'close': function(fn) {
-			this.options.srcNode.hide();
-			this._trigger('close');
-		}
-	});
+    'close': function(fn) {
+      this.options.srcNode.hide();
+      this._trigger('close');
+    }
+  });
 
 })(jQuery);
